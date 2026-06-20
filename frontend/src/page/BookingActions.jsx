c791ca835e763411910fec2a523779efabdc6bbe
lebathuan03052005-coder <lebathuan03052005@gmail.com>
2026-06-20 13:21:53 +0700

    update

--- frontend/src/page/BookingActions.jsx
+++ frontend/src/page/BookingActions.jsx
@@ -1,6 +1,6 @@
 // Ví dụ dùng trong trang quản lý booking (ví dụ BookingDetail.jsx, AdminBookings.jsx...)
 
-import { confirmBooking, cancelBooking } from "/bookingAPI";
+import { confirmBooking, cancelBooking } from "./BookingActions.jsx";
 
 function BookingActions({ booking, onUpdated }) {
   const handleConfirm = async () => {
